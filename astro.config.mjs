51402ed36b22386b823273da468c8fb486748e4f
SofiDev <angiemeow89@gmail.com>
2025-08-08 01:52:16 -0600

    🚧 Hero section in progress
    Co-authored-by: Stron - Jose Luis B. Leyva <elstron@users.noreply.github.com>
    Co-authored-by: Ricardo Vega G <contact@rickytodev.xyz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sofidev.blog/",
+  adapter: cloudflare({
+    imageService: "compile"
+  }),
+  output: 'server',
+});
