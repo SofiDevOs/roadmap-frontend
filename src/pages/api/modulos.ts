3b02e8e97c500c3f815d923e18733cee83c0bbde
SofiDev <angiemeow89@gmail.com>
2025-08-14 00:16:55 -0600

    🎉 Se crea directorio API

---
+++ src/pages/api/modulos.ts
@@ -0,0 +1,39 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({request}) => {
+
+    if(!request.referrer.includes("sofidev.blog")) return new Response("unauthorized", {status:401})
+
+    console.log(
+        request
+    )
+
+    let obj = {
+        hola: "mundo"
+    }
+
+    return new Response(
+        JSON.stringify(obj),{
+            status: 200
+        }
+    )
+};
+
+export const GET: APIRoute = async ({request}) => {
+
+    if(!request.referrer.includes("sofidev.blog")) return new Response("unauthorized", {status:401})
+
+    console.log(
+        request
+    )
+
+    let obj = {
+        hola: "mundo"
+    }
+
+    return new Response(
+        JSON.stringify(obj),{
+            status: 200
+        }
+    )
+};
