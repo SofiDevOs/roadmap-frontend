bc9cabea12eceeb89a33a017507281f1d636c6df
ElStron <luisstron45@gmail.com>
2025-08-23 00:47:10 -0600

    Fix: Se arreglan los imports

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
-import { UserRepository } from "backend/user/repositories/userRepository";
-import { UserController } from "backend/user/controllers/userController";
-import { createUserService } from "backend/user/services/registerUser";
+import { UserRepository } from "@backend/user/repositories/userRepository";
+import { UserController } from "@backend/user/controllers/userController";
+import { createUserService } from "@backend/user/services/registerUser";
 const userRepository = new createUserService(new UserRepository());
 const userController = new UserController(userRepository);
 
