d713412d65e50f6d749d8cc4372eaff09b0e1e37
SofiDev <angiemeow89@gmail.com>
2025-08-23 18:08:48 -0600

    fix: resolve merge conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from "@astrojs/cloudflare";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sofidev.blog/",
+
   adapter: cloudflare({
     imageService: "compile"
   }),
+
   output: 'server',
+  integrations: [react()],
 });
