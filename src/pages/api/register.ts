6480b8148db5103ebfd24da333fe245beee5103c
SofiDev <angiemeow89@gmail.com>
2025-09-03 13:49:15 -0600

    💄 Style(cards Roadmap): Se agregan tags dinamicas y colores pcustomizados a las cards

--- src/pages/api/register.ts
+++
@@ -1,5 +0,0 @@
-import type { APIRoute, AstroSharedContext } from "astro";
-import { userController } from "@backend/user/dependencies";
-
-export const GET: APIRoute = (context: AstroSharedContext) => userController.getUserById(context);
-export const POST: APIRoute = (context: AstroSharedContext) =>  userController.register(context);
--- src/pages/api/verify.ts
+++
@@ -1,4 +0,0 @@
-import type { APIRoute, AstroSharedContext } from "astro";
-import { userController } from "@backend/user/dependencies";
-
-export const GET: APIRoute = (context: AstroSharedContext) =>  userController.verify(context);
