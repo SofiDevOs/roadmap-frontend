09f10b7d7d85ebeb5c0a880208de969f2acd1e3d
ElStron <luisstron45@gmail.com>
2025-10-18 16:28:32 -0600

    feat(e2e): agrega prueba de creación de cursos y configura baseUrl en Cypress
    - Se añade el archivo de prueba E2E para la creación de cursos en el dashboard.
    - Se configura la propiedad baseUrl en cypress.config.js para facilitar las pruebas.

---
+++ cypress/e2e/dashboard/courses/create.cy.js
@@ -0,0 +1,53 @@
+
+describe('/courses/create: Create courses', () => {
+  beforeEach(() => {
+    cy.session('user', () => {
+ cy.request({
+        method: 'POST',
+        url: 'http://localhost:8787/auth/login',
+        form: true,
+        body: {
+          email: 'testuser@example.com',
+          password: 'password123',
+        },
+        followRedirect: false,      
+      }).then((response) => {
+        expect(response.status).to.eq(302);       
+      });
+    });
+
+    cy.intercept('POST','**/courses',{
+      statusCode:200,
+      body: {
+        message: 'Curso creado con éxito',
+      }
+    }).as('createCourse');
+     
+    cy.visit('/dashboard/cursos/create');
+  })
+  
+  it('should display course creation form', () => {
+    cy.get('form#create-course').should('be.visible');
+  })
+
+  it('There should be an invisible input with the user ID in the value.', ()=>{
+    cy.get('form#create-course').should('be.visible');
+    cy.get("input[name='user_id']").should('have.attr', 'hidden', );
+  })
+
+  it('should create a new course with valid data', () => {
+    cy.get("input[name='title']").type('Curso de Prueba');
+    cy.get("textarea[name='description']").type('Descripción del curso de prueba');
+
+    cy.get("button[type='submit']").and('have.text', 'Agregar').click();
+    
+    cy.wait('@createCourse').should(({response})=>{
+      expect(response.statusCode).to.eq(200);
+    })
+  })
+
+  it('should show error when required fields are missing', () => {
+    cy.get("button[type='submit']").and('have.text', 'Agregar').click();
+  })
+
+})
