832b3cb838f7c65cc00e6e7deec6e125b07e7e78
ElStron <luisstron45@gmail.com>
2025-10-22 17:18:58 -0600

    feat(middleware): agrega control de acceso por roles y página de dashboard para estudiantes
    - Implementa lógica de middleware para redirigir usuarios según su rol (admin, student, teacher)
    - Crea la página inicial del dashboard para estudiantes mostrando su nombre

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,37 @@
 import { isLoggedIn } from "@utils/auth/isLoggedIn";
 import { defineMiddleware } from "astro:middleware";
+import { sequence } from "astro:middleware";
+
+export const USER_ROLES = {
+  admin: {
+    path: "/dashboard/"
+  },
+  student: {
+    path:"/dashboard/student"
+  },   
+  teacher: {
+    path:"/dashboard/teacher"
+  },
+};
 
 
-export const onRequest = defineMiddleware(async ({originPathname, cookies, locals, redirect}, next) => {
-  
-  if(!originPathname.startsWith('/dashboard')) return next()
+export const auth = defineMiddleware(
+  async ({ originPathname, cookies, locals, redirect }, next) => {
+    
+    if ( !originPathname.startsWith("/dashboard") )
+      return next();
 
-  const user = await isLoggedIn(cookies)
-  if(!user) return redirect('/access/login')
-  
-  locals.user = user
-  return next()
+    const user = await isLoggedIn(cookies);
 
-})
+    if (!user)
+      return redirect("/access/login");
+
+    if ( !originPathname.startsWith(USER_ROLES[user.role].path) )
+      return redirect(USER_ROLES[user.role].path);
+
+    locals.user = user
+    return next();
+  }
+);
+
+export const onRequest = sequence(auth);
