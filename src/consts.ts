0be84f8a1d95f909a72864dd5c4e13d7f8095c3b
SofiDevO <angiemeow89@gmail.com>
2025-10-25 00:08:39 -0600

    refactor auth form

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 import { apiClient } from "./utils/apiClient";
 
-const BASE_API_URL  =  import.meta.env.DEV ? 'http://localhost:8787' : 'https://api.sofidev.top';
+const BASE_API_URL  =  import.meta.env.BACKEND_URL;
 
 export const api = apiClient(BASE_API_URL);
 
