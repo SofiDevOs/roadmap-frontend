02e733395e8853b829d2b8ac09555b00cd1dc3be
ElStron <luisstron45@gmail.com>
2025-11-17 14:52:27 -0600

    feat(auth): mejora el manejo de cookies en logout y verificación de sesión
    - Elimina el token de acceso usando opciones avanzadas de cookies (domain, secure, httpOnly, sameSite).
    - Añade utilitario baseDomain para obtener el dominio base dinámicamente.
    - Actualiza isLoggedIn para borrar la cookie con opciones seguras si el JWT es inválido.
    - Refactoriza el manejo de dominio según entorno (desarrollo o producción).

--- src/utils/auth/isLoggedIn.ts
+++ src/utils/auth/isLoggedIn.ts
@@ -1,22 +1,31 @@
-import * as jose from 'jose'
-import type {AstroCookies} from 'astro'
+import * as jose from "jose";
+import type { AstroCookies } from "astro";
+import { baseDomain } from "@utils/baseDomain";
 
-const { JWT_SECRET } = import.meta.env;
+const { JWT_SECRET, BACKEND_URL } = import.meta.env;
 
 export async function isLoggedIn(cookies: AstroCookies) {
-    const hasToken = cookies.get('access_token');
-    if (!hasToken) return false;
+  const hasToken = cookies.get("access_token");
+  if (!hasToken) return false;
 
-    const secret = new TextEncoder().encode(JWT_SECRET);
-    try {
-        const { payload } = await jose.jwtVerify(hasToken.value || '', secret);
-        if (!payload) throw new Error('Invalid token payload');
-        return payload;
+  const secret = new TextEncoder().encode(JWT_SECRET);
+  const domain = import.meta.env.DEV
+    ? "localhost"
+    : `.${baseDomain(new URL(BACKEND_URL).hostname)}`;
 
-    } catch (error) {
-        console.error('JWT verification error:', error);
-        cookies.delete('access_token');
-        return false;
-    }
+  try {
+    const { payload } = await jose.jwtVerify(hasToken.value || "", secret);
+    if (!payload) throw new Error("Invalid token payload");
+    return payload;
+  } catch (error) {
+    console.error("JWT verification error:", error);
+    cookies.delete("access_token", {
+      path: "/",
+      domain: domain,
+      secure: true,
+      httpOnly: true,
+      sameSite: "none",
+    });
+    return false;
+  }
 }
-
