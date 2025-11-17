65d2f68d01ecba156fe6f2d524b73526a5893c9b
ElStron <luisstron45@gmail.com>
2025-11-17 13:54:20 -0600

    feat(auth): mejora verificación JWT y manejo de errores
    - Se agrega validación para existencia de access_token antes de verificar JWT.
    - Se mejora el manejo de errores mostrando detalles en consola.
    - Se elimina el token inválido de las cookies en caso de error.
    - Se agrega el archivo public/.assetsignore.

--- src/utils/auth/isLoggedIn.ts
+++ src/utils/auth/isLoggedIn.ts
@@ -1,19 +1,22 @@
 import * as jose from 'jose'
 import type {AstroCookies} from 'astro'
-// JWT_SECRET
+
 const jwtSecret = import.meta.env.JWT_SECRET;
 
 export async function isLoggedIn(cookies: AstroCookies) {
     const hasToken = cookies.get('access_token');
+    if (!hasToken) return false;
 
-    if(!hasToken) return false
     const secret = new TextEncoder().encode(jwtSecret);
     try {
         const { payload } = await jose.jwtVerify(hasToken.value || '', secret);
         if (!payload) throw new Error('Invalid token payload');
         return payload;
+
     } catch (error) {
+        console.error('JWT verification error:', error);
         cookies.delete('access_token');
         return false;
     }
 }
+
