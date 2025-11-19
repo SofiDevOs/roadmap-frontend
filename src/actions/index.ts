0deef7b80c68382b96f496f186a5d8b2a3ab4e72
ElStron <luisstron45@gmail.com>
2025-11-18 18:14:02 -0600

    feat: migrar llamadas API a astro:actions y mejorar manejo de errores
    Se reemplazaron las llamadas directas a la función api por el uso de astro:actions en los formularios y componentes administrativos.
    Ahora el manejo de errores utiliza ActionError para mayor claridad y consistencia.
    Se actualizaron los formularios de cursos, lecciones y roadmaps para usar la nueva estructura y notificaciones de error.
    Además, se ajustó la subida de archivos para utilizar URLs dinámicas.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,31 +1,50 @@
-import { defineAction } from 'astro:actions';
+import { defineAction, ActionError, type ActionErrorCode } from 'astro:actions';
 import { z } from 'astro:schema';
 import { api } from 'src/consts';
 import { SignJWT } from 'jose';
-const secret = new TextEncoder().encode(import.meta.env.JWT_SECRET);
+
+const secret = new TextEncoder().encode(
+  import.meta.env.JWT_SECRET
+);
+
+const statuses = {
+  400: 'BAD_REQUEST',
+  403: 'FORBIDDEN',
+  404: 'NOT_FOUND',
+  401: 'UNAUTHORIZED',
+  500: 'INTERNAL_SERVER_ERROR'
+}
+
 export const server = {
   api: defineAction({
     input: z.object({
       resource: z.string(),
-      data: z.record(z.any())
+      data: z.record(z.any()).optional(),
+      method: z.enum(['GET', 'POST', 'PUT', 'DELETE']).default('GET').optional(),
+      id: z.string().optional()
     }),
     handler: async (input) => {
-      const token = await new SignJWT({}).setProtectedHeader({alg: "HS256"}).sign(secret);
-      console.log(input.data) 
-      const [data, error, status] = await api(input.resource)()({
-        method: 'POST',
+      console.log('API Action Input:', input);
+      const token = await new SignJWT({})
+        .setProtectedHeader({alg: "HS256"})
+        .sign(secret);
+      
+      const [data, error, status] = await api(input.resource)(input.id ?? '')({
+        method: input.method,
         headers: {
           Authorization: `Bearer ${token}`
         },
-        body: JSON.stringify(input.data)
+        body: JSON.stringify(input.data) || undefined
       });
-      
-      // Convert Error object to plain object for serialization
-      return {
-        data,
-        error: error ? { message: error.message, name: error.name } : null,
-        status
-      };
+
+      if( error || statuses[status as keyof typeof statuses ?? 500] ) 
+        throw new ActionError({ 
+          message: error?.message || 'An error occurred', 
+          code: statuses[status as keyof typeof statuses] as ActionErrorCode 
+            || 'INTERNAL_SERVER_ERROR' 
+        });
+
+      return { data, status };
     }
   })
 }
