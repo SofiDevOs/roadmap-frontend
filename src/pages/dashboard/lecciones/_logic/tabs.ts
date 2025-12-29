d504106f6c6907196209de2e482e66a92ab5f893
SofiDev <angiemeow89@gmail.com>
2025-12-29 01:57:44 -0600

    🚧 Work in progress: Creando formulario trivia

--- src/pages/dashboard/lecciones/_logic/tabs.ts
+++ src/pages/dashboard/lecciones/_logic/tabs.ts
@@ -5,17 +5,21 @@ tabsParent?.addEventListener("click", (e) => {
   const target = e.target as HTMLElement;
 
   if (target.tagName !== "BUTTON") return;
+  if (target.tagName === "BUTTON") {
+    const buttons = tabsParent.querySelectorAll("button");
+    buttons.forEach((btn) => btn.classList.remove("is-active"));
+    target.classList.add("is-active");
+  }
 
   resetPanels();
 
   const panel = document.querySelector(
     `[data-id="${target.id}"]`
   ) as HTMLElement;
-
   panel.hidden = false;
 });
 function resetPanels() {
   const panels = document.querySelectorAll(".panel") as NodeListOf<HTMLElement>;
 
-  panels.forEach((panel) => panel.hidden = true );
-};
+  panels.forEach((panel) => (panel.hidden = true));
+}
